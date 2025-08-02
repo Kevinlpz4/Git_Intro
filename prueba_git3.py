b0f43f61a83c37470d7057ff1ffa62dd28738889
zDoggedz <kevinlpz0405@gmail.com>
2025-08-01 22:05:02 -0500

    Ultima iteracion

--- prueba_git3.py
+++ prueba_git3.py
@@ -4,9 +4,4 @@ print("este es mi tercer archivo")
 
 print(" Version 3.0")
 
-<<<<<<< HEAD
-print(" Version 3.0 mejoraday lista para lanzar")
-=======
-print(" Version 3.0 mejorada")
-
->>>>>>> Login
+print(" Version 3.0 mejorada y lista para lanzar")
